451773e9f96eff387b636c65bc18afefe41b824c
krebsni <st151522@stud.uni-stuttgart.de>
2023-06-26 15:23:07 +0200

    working example for averaging temperatures of two kafka unbounded inputs with windowing strategy

--- beam-template/src/main/java/{{PACKAGE_NAME}}/TestBeamPipeline.java
+++ beam-template/src/main/java/{{PACKAGE_NAME}}/TestBeamPipeline.java
@@ -4,10 +4,15 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.beam.sdk.transforms.*;
+import org.apache.beam.sdk.transforms.windowing.*;
+import org.apache.beam.sdk.values.*;
+import org.checkerframework.checker.initialization.qual.Initialized;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.UnknownKeyFor;
+import org.joda.time.Instant;
 import org.json.JSONObject;
 import org.json.JSONTokener;
-
-import netscape.javascript.JSException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -22,127 +27,105 @@ import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.options.Validation.Required;
-import org.apache.beam.sdk.transforms.Count;
-import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.MapElements;
-import org.apache.beam.sdk.transforms.ParDo;
-import org.apache.beam.sdk.transforms.SimpleFunction;
-import org.apache.beam.sdk.transforms.Values;
-import org.apache.beam.sdk.transforms.windowing.Window;
-import org.apache.beam.sdk.transforms.windowing.FixedWindows;
-import org.apache.beam.sdk.transforms.Mean;
-import org.apache.beam.sdk.values.TypeDescriptors;
 import org.joda.time.Duration;
 
 
-import org.apache.beam.sdk.transforms.Flatten;
-import org.apache.beam.sdk.values.KV;
-import org.apache.beam.sdk.values.PCollectionList;
-import org.apache.beam.sdk.values.PCollection;
 import org.apache.kafka.common.TopicPartition;
-import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
+
 public class TestBeamPipeline {
-  private static final Logger LOG = LogManager.getLogger(TestBeamPipeline.class);
-  // private static final Logger LOG = LoggerFactory.getLogger(TestBeamPipeline.class);
+    private static final Logger LOG = LogManager.getLogger(TestBeamPipeline.class);
+    // private static final Logger LOG = LoggerFactory.getLogger(TestBeamPipeline.class);
 
-  public interface IoTStreamingOptions extends PipelineOptions {
+    public interface IoTStreamingOptions extends PipelineOptions {
 
-    @Description("Kafka bootstrap servers, comma separated list")
-    @Default.String("localhost:9092")
-    String getKafkaBootstrapServers();
+        @Description("Kafka bootstrap servers, comma separated list")
+        @Default.String("localhost:9092")
+        String getKafkaBootstrapServers();
 
-    void setKafkaBootstrapServers(String kafkaBootstrapServers);
+        void setKafkaBootstrapServers(String kafkaBootstrapServers);
 
-    @Description("Kafka topic to read from")
-    @Required
-    String getKafkaTopic();
+        @Description("Kafka topic to read from")
+        @Required
+        String getKafkaTopic();
 
-    void setKafkaTopic(String kafkaTopic);
+        void setKafkaTopic(String kafkaTopic);
 
-    @Description("Path of the file to write to")
-    @Default.String("output.txt")
-    String getOutputFile();
+        @Description("Path of the file to write to")
+        @Default.String("output.txt")
+        String getOutputFile();
 
-    void setOutputFile(String outputFile);
-  }
+        void setOutputFile(String outputFile);
+    }
 
-  public static void main(String[] args) {
-    // Create the pipeline options
-    IoTStreamingOptions options = PipelineOptionsFactory.fromArgs(args).as(IoTStreamingOptions.class);
+    private static PTransform<@UnknownKeyFor @NonNull @Initialized PBegin, @UnknownKeyFor @NonNull @Initialized PCollection<@UnknownKeyFor @NonNull @Initialized KV<String, String>>> kafkaRead(IoTStreamingOptions options, String test, Map<String, Object> consumerConfig) {
+        return KafkaIO.<String, String>read()
+                .withBootstrapServers(options.getKafkaBootstrapServers())
+                .withTopicPartitions(
+                        Collections.singletonList(new TopicPartition(test, 0))
+                ) // TODO: support multiple partitions
+                .withKeyDeserializer(StringDeserializer.class)
+                .withValueDeserializer(StringDeserializer.class)
+                .withConsumerConfigUpdates(consumerConfig) // TODO: needed?
+                .withoutMetadata();
+    }
 
-    // Create the pipeline
-    Pipeline pipeline = Pipeline.create(options);
+    private static <T> Window<T> window() {
+        return Window.<T>into(FixedWindows.of(Duration.standardSeconds(10)))
+                .triggering(Repeatedly.forever(AfterProcessingTime.pastFirstElementInPane().plusDelayOf(Duration.standardSeconds(10))))
+                .withAllowedLateness(Duration.ZERO)
+                .discardingFiredPanes();
+    }
 
-    final Map<String, Object> consumerConfig = new HashMap<>();
-    consumerConfig.put("auto.offset.reset", "earliest"); // TODO: make configurable
+    public static void main(String[] args) {
+        // Create the pipeline options
+        IoTStreamingOptions options = PipelineOptionsFactory.fromArgs(args).as(IoTStreamingOptions.class);
 
-    // Read from Kafka
-    PCollection<KV<String, String>> input1 = pipeline
-        .apply("Read from Kafka", KafkaIO.<String, String>read()
-            .withBootstrapServers(options.getKafkaBootstrapServers())
-            .withTopicPartitions(
-                Collections.singletonList(new TopicPartition("test", 0))
-            ) // TODO: support multiple partitions
-            .withKeyDeserializer(StringDeserializer.class)
-            .withValueDeserializer(StringDeserializer.class)
-            // bound con  
-            .withMaxNumRecords(10)
-            .withConsumerConfigUpdates(consumerConfig) // TODO: needed?
-            .withoutMetadata()) // TODO: support metadata
-        .apply(Window.<KV<String, String>>into(FixedWindows.of(Duration.standardSeconds(5))));
-            
-    PCollection<KV<String, String>> input2 = pipeline
-        .apply("Read from Kafka", KafkaIO.<String, String>read()
-            .withBootstrapServers(options.getKafkaBootstrapServers())
-            .withTopicPartitions(
-                Collections.singletonList(new TopicPartition("test1", 0))
-            ) // TODO: support multiple partitions
-            .withKeyDeserializer(StringDeserializer.class)
-            .withValueDeserializer(StringDeserializer.class)
-            .withMaxNumRecords(10)
-            .withConsumerConfigUpdates(consumerConfig) // TODO: needed?
-            .withoutMetadata()) // TODO: support metadata
-        .apply(Window.<KV<String, String>>into(FixedWindows.of(Duration.standardSeconds(5))))
-        // print out keys and values in system out
-        .apply(ParDo.of(new DoFn<KV<String, String>, KV<String, String>>() {
-          @ProcessElement
-          public void processElement(ProcessContext c) {
-            System.out.println("Processing element: " + c.element());
-            c.output(c.element());
-          }
-        }));
+        // Create the pipeline
+        Pipeline pipeline = Pipeline.create(options);
 
-    PCollection<String> merged = PCollectionList.of(input1).and(input2)
-        .apply(Flatten.<KV<String, String>>pCollections())
-        .apply(Values.create());
+        final Map<String, Object> consumerConfig = new HashMap<>();
+        consumerConfig.put("auto.offset.reset", "earliest"); // TODO: make configurable
+
+        // Read from Kafka
+        PCollection<KV<String, String>> input1 = pipeline
+                .apply("Read from Kafka", kafkaRead(options, "test", consumerConfig)); // TODO: support metadata
+        PCollection<KV<String, String>> input2 = pipeline
+                .apply("Read from Kafka", kafkaRead(options, "test1", consumerConfig)); // TODO: support metadata
+
+        PCollection<String> merged = PCollectionList.of(input1).and(input2)
+                .apply(Flatten.<KV<String, String>>pCollections())
+                .apply(window())
+                .apply(Values.create());
 
 
-    PCollection<Integer> temp = merged.apply("Parse Temperature", 
-            ParDo.of(
-                new DoFn<String, Integer>() {
-                  @ProcessElement
-                  public void processElement(ProcessContext c) {
-                    try {
-                      LOG.info("Processing element: " + c.element());
-                      JSONTokener tokener = new JSONTokener(c.element());
-                      JSONObject entry = new JSONObject(tokener);
-                      Integer temperature = (Integer) entry.get("temperature");
-                      c.output(temperature);
-                    } catch (Exception e) {
-                      System.out.println("Error parsing JSON: " + e.getMessage()); // TODO: replace with logger
-                    }
-                  }
-                }));
-    
-    PCollection<Double> avgTemp = temp.apply("Average Temperature", Mean.<Integer>globally().withoutDefaults());
+        PCollection<Integer> temp = merged.apply("Parse Temperature",
+                ParDo.of(
+                        new DoFn<String, Integer>() {
+                            @ProcessElement
+                            public void processElement(@Element String element, OutputReceiver<Integer> out, BoundedWindow window, @Timestamp Instant timestamp) {
+                                try {
+                                    System.out.println("[" + timestamp.toString() + "] Processing element: " + element);
+                                    JSONTokener tokener = new JSONTokener(element);
+                                    JSONObject entry = new JSONObject(tokener);
+                                    Integer temperature = (Integer) entry.get("temperature");
+                                    out.output(temperature);
+                                } catch (Exception e) {
+                                    System.out.println("Error parsing JSON: " + e.getMessage()); // TODO: replace with logger
+                                }
+                            }
+                        }));
 
-    avgTemp.apply("Format Output", MapElements.into(TypeDescriptors.strings())
-      .via(d -> Double.toString(d)))
-      .apply("Write to File", TextIO.write().to(options.getOutputFile()).withWindowedWrites());
-    
-    // Run the pipeline
-    pipeline.run();
-  }
+        PCollection<Double> avg = temp.apply("Average Temperature in Window", Mean.<Integer>globally().withoutDefaults());
+
+        // TODO: write output to delta lake
+        avg.apply("Format Output", MapElements.into(TypeDescriptors.strings())
+            .via(d -> Double.toString(d)))
+            .apply("Write to File", TextIO.write().to(options.getOutputFile()).withWindowedWrites());
+
+
+        // Run the pipeline
+        pipeline.run();
+    }
 }
